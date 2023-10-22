b9e27534c461cfdcc94b6b22f706bc117852d695
William Martinez <wrmartinez709@gmail.com>
2023-10-22 15:33:28 -0400

    add comments explaining steps to solution

--- program_structure/looping_a_triangle.js
+++ program_structure/looping_a_triangle.js
@@ -9,11 +9,14 @@ Write a loop that makes seven calls to console.log to output the following trian
 ######
 */
 
+// Create a binding for the number of iteration
 let num = 1;
+// Create a binding for the developing shape being created
 let shape = "";
-
+// Write a loop that will add a "#" to the shape binding for a total of seven iterations
 while (num < 8) {
   shape += "#";
+  // Log the shape on each iteration
   console.log(shape);
   num++;
 }
