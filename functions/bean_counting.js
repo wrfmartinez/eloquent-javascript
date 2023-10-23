9090745ad21dcbd86da4658b4961725660834a61
William Martinez <wrmartinez709@gmail.com>
2023-10-22 21:43:43 -0400

    implement countChar function and refactor countBs function

--- functions/bean_counting.js
+++ functions/bean_counting.js
@@ -6,25 +6,6 @@ Next, write a function called countChar that behaves like countBs, except it tak
 Rewrite countBs to make use of this new function
 */
 
-// console.log("string"["string".length - 1]);
-
-// 1. Create a binding for the amount of uppercase "B"s found in a string
-// 2. Write a loop that iterates through the strings letters
-// 3. Compare each letter to uppercase "B".
-//    if it is a match increase counter binding by 1
-// 4. Return amount found
-const countBs = function(string) {
-  let count = 0;
-
-  for (i = 0; i < string.length; i++) {
-    if (string[i] == "B") {
-      count++;
-    }
-  }
-
-  return count;
-}
-
 const countChar = function(string, char) {
   let count = 0;
 
@@ -36,3 +17,10 @@ const countChar = function(string, char) {
 
   return count;
 }
+
+const countBs = function(string) {
+  return countChar(string, "B")
+}
+
+// console.log(countBs("BBC"));
+// console.log(countChar("kakkerlak", "k"));
